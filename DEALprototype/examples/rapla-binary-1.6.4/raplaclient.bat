cb678158eeaf5c814b3bd789a800d0ffafc50cfb
mishelka <mishelka@b6a615d9-716e-4a19-ae32-0c32c17d3d78>
2013-07-04 17:53:37 +0000

    - some non-functional example libaries deleted or changed

--- DEALprototype/examples/rapla-binary-1.6.4/raplaclient.bat
+++
@@ -1,3 +0,0 @@
-cls
-echo webapp/webclient/raplaclient.xconf is used check remote server settings
-call rapla.bat client %1 %2 %3 %4
--- DEALprototype/examples/rapla-binary-1.6.4/raplaimport.bat
+++
@@ -1,5 +0,0 @@
-cls
-echo raplaserver.xconf is used check file and sql settings
-pause
-call rapla.bat -c .\webapp\WEB-INF\raplaserver.xconf import
-pause
