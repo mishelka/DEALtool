980624e013115fe021c765ea912283e00b1ded0b
mishelka <mishelka@b6a615d9-716e-4a19-ae32-0c32c17d3d78>
2013-10-24 09:31:51 +0000

    minor changes

--- DEALprototype/src/gui/analyzer/observable/AppObservable.java
+++ DEALprototype/src/gui/analyzer/observable/AppObservable.java
@@ -4,10 +4,9 @@ import gui.model.application.scenes.Scene;
 
 import java.util.Observable;
 
-@SuppressWarnings("rawtypes")
 public abstract class AppObservable extends Observable {
-	public abstract void notifySceneAdded(Scene scene);
-	public abstract void notifySceneRemoved(Scene scene);
-	public abstract void notifySceneEdited(Scene scene);
+	public abstract void notifySceneAdded(Scene<?> scene);
+	public abstract void notifySceneRemoved(Scene<?> scene);
+	public abstract void notifySceneEdited(Scene<?> scene);
 	public abstract void notifyAppNameChanged();
 }
