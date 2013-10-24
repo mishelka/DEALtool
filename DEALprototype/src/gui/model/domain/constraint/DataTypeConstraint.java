8aaf2408ce1d2c06585755e1b635aa0e1c28a908
mishelka <mishelka@b6a615d9-716e-4a19-ae32-0c32c17d3d78>
2013-10-24 07:20:51 +0000

    javadoc

--- DEALprototype/src/gui/model/domain/constraint/DataTypeConstraint.java
+++ DEALprototype/src/gui/model/domain/constraint/DataTypeConstraint.java
@@ -1,16 +1,32 @@
 package gui.model.domain.constraint;
 
+/**
+ * DataTypeConstraint represents the data type of a term.
+ * 
+ * @author Michaela Bacikova, Slovakia,
+ * michaela.bacikova@tuke.sk
+ */
 public class DataTypeConstraint extends Constraint {
+	/** The data type of the term, which has this constraint. */
 	private DataType dataType = null;
 	
+	/**
+	 * @param dataType the data type of the term, which has the constraint
+	 */
 	public DataTypeConstraint(DataType dataType) {
 		this.dataType = dataType;
 	}
 	
+	/**
+	 * @return the data type of the term, which has the constraint
+	 */
 	public DataType getType() {
 		return dataType;
 	}
 	
+	/**
+	 *  @param dataType the data type of the term, which has the constraint
+	 */
 	public void setType(DataType dataType) {
 		this.dataType = dataType;
 	}
--- DEALprototype/src/gui/model/domain/constraint/Length.java
+++ DEALprototype/src/gui/model/domain/constraint/Length.java
@@ -1,19 +1,37 @@
 package gui.model.domain.constraint;
 
+/**
+ * Defines the maximum or minimum length of a string data type term.
+ * 
+ * @author Michaela Bacikova, Slovakia,
+ * michaela.bacikova@tuke.sk
+ */
 public class Length extends Constraint {
+	/** Minimum length of a string data type term. */
     private final int minLength;
 
+    /** Maximum length of a string data type term. */
     private final int maxLength;
 
+    /**
+     * @param minLength minimum length of a string data type term. 
+     * @param maxLength maximum length of a string data type term. 
+     */
     public Length(int minLength, int maxLength) {
         this.minLength = minLength;
         this.maxLength = maxLength;
     }
 
+    /**
+     * @return maximum length of a string data type term. 
+     */
     public int getMaxLength() {
         return maxLength;
     }
 
+    /**
+     * @return minimum length of a string data type term. 
+     */
     public int getMinLength() {
         return minLength;
     }
