33056200518dca134541e2d1104fd66bf2707a7a
mishelka <mishelka@b6a615d9-716e-4a19-ae32-0c32c17d3d78>
2013-10-25 13:05:28 +0000

    javadoc

--- DEALprototype/src/gui/analyzer/observable/AppObservable.java
+++ DEALprototype/src/gui/analyzer/observable/AppObservable.java
@@ -4,9 +4,29 @@ import gui.model.application.scenes.Scene;
 
 import java.util.Observable;
 
+/**
+ * Observer for the Application class.
+ * @author Michaela Bacikova, Slovakia,
+ * michaela.bacikova@tuke.sk
+ */
 public abstract class AppObservable extends Observable {
+	/**
+	 * Notifies observers when a scene was added to the Applications' scene list.
+	 * @param scene the scene, which was added to the Applications' scene list
+	 */
 	public abstract void notifySceneAdded(Scene<?> scene);
+	/**
+	 * Notifies observers when a scene was removed from the Applications' scene list.
+	 * @param scene the scene which was removed
+	 */
 	public abstract void notifySceneRemoved(Scene<?> scene);
+	/**
+	 * Notifies observers when a scene in the Applications' scene list was edited.
+	 * @param scene the scene which was edited
+	 */
 	public abstract void notifySceneEdited(Scene<?> scene);
+	/**
+	 * Notifies observers when the application name is changed.
+	 */
 	public abstract void notifyAppNameChanged();
 }
--- DEALprototype/src/gui/analyzer/observable/ApplicationEvent.java
+++ DEALprototype/src/gui/analyzer/observable/ApplicationEvent.java
@@ -2,35 +2,75 @@ package gui.analyzer.observable;
 
 import gui.model.application.scenes.Scene;
 
+/**
+ * The event type, which is used in AppObservable.
+ * 
+ * @author Michaela Bacikova, Slovakia,
+ * michaela.bacikova@tuke.sk
+ */
 @SuppressWarnings("rawtypes")
 public class ApplicationEvent {
-	private Scene target;
+	/** The source scene - the scene that fired the event. */
+	private Scene source;
+	/** The type of change that occured: Scene was added, edited, removed or application name was changed. */
 	private ApplicationChangeState changeState;
+	/** The name of the application, in which the event was fired. */
 	private String applicationName;
 	
-	public ApplicationEvent(Scene target, ApplicationChangeState changeState) {
-		this.target = target;
+	/** Constructor for the state change events */
+	public ApplicationEvent(Scene source, ApplicationChangeState changeState) {
+		this.source = source;
 		this.changeState = changeState;
 	}
 	
+	/** Constructor for the name change events */
 	public ApplicationEvent(String newName) {
 		this.applicationName = newName;
 		changeState = ApplicationChangeState.NAME_CHANGED;
 	}
 	
-	public Scene getTargetScene() {
-		return target;
+	/**
+	 * @return the scene which fired the event
+	 */
+	public Scene getSourceScene() {
+		return source;
 	}
 	
+	/**
+	 * @return the change, which was performed
+	 */
 	public ApplicationChangeState getChangeState() {
 		return changeState;
 	}
 	
+	/**
+	 * @return the name of the application, in which the event was fired
+	 */
 	public String getApplicationName() {
 		return applicationName;
 	}
 	
+	/**
+	 * Stores the type of change, that was performed as a source of this event.
+	 * @author Michaela Bacikova, Slovakia,
+	 * michaela.bacikova@tuke.sk
+	 */
 	public enum ApplicationChangeState {
-		ADDED, EDITED, REMOVED, NAME_CHANGED;
+		/**
+		 * A scene was added into the Applications' scene list.
+		 */
+		ADDED, 
+		/**
+		 * A scene was edited in the Applications' scene list.
+		 */
+		EDITED, 
+		/**
+		 * A scene was removed from the Applications' scene list.
+		 */
+		REMOVED, 
+		/**
+		 * A name of the application was changed.
+		 */
+		NAME_CHANGED;
 	}
 }
