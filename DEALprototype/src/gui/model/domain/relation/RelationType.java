e57926bb84f1269808f16e8f9c80e4ee30dd488f
mishelka <mishelka@b6a615d9-716e-4a19-ae32-0c32c17d3d78>
2013-11-11 09:23:37 +0000

    javadoc

--- DEALprototype/src/gui/model/domain/relation/RelationType.java
+++ DEALprototype/src/gui/model/domain/relation/RelationType.java
@@ -5,5 +5,6 @@ public enum RelationType {
 	AND,
 	MUTUALLY_EXCLUSIVE,
 	MUTUALLY_NOT_EXCLUSIVE;
-	//TODO: pridat dalsie typy relacii po tom, ako si pozriem Protege a podla toho, ci bude mozne tieto typy ziskat z rozhrania
+	//TODO: pridat dalsie typy relacii po tom, ako si pozriem Protege
+	//a podla toho, ci bude mozne tieto typy ziskat z rozhrania
 }
